62e4f6f806885475eae26aa00fa51469aa544a8d
RifqohR1903056 <rifqohrojiah45@gmail.com>
2022-06-03 10:52:12 +0700

    menambahkan views kontak

---
+++ application/views/v_contact.php
@@ -0,0 +1,55 @@
+ <!-- Contact Section Begin -->
+    <section class="contact spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 col-md-6 col-sm-5">
+                    <div class="contact__widget">
+                        <div class="contact__widget__item">
+                            <h4>Contact Us</h4>
+                            <ul>
+                                <li>(061) 456-78</li>
+                                <li>istiqomah.info@gmail.com</li>
+                            </ul>
+                        </div>
+                        <div class="contact__widget__item">
+                            <h4>Address</h4>
+                            <p>selayang Pandang<br />Indonesia</p>
+                        </div>
+                        <div class="contact__widget__time">
+                            <h4>Opentime</h4>
+                            <div class="row">
+                                <div class="col-lg-6 col-md-6 col-sm-6">
+                                    <div class="contact__widget__time__item">
+                                        <ul>
+                                            <li>Monday - Friday</li>
+                                            <li><span>everyday</span></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-6">
+                                    <div class="contact__widget__time__item">
+                                        <ul>
+                                            <li>Saturday - Sunday</li>
+                                            <li><span>everyday</span></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 offset-lg-1 col-md-6 col-sm-7">
+                    <div class="contact__form">
+                        <h2>Your question?</h2>
+                        <form action="#">
+                            <input type="text" placeholder="Your Name">
+                            <input type="text" placeholder="Email">
+                            <textarea placeholder="Your Message"></textarea>
+                            <button type="submit">Send Message</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Contact Section End -->
