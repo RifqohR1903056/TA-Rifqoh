b8b6ce40f840ed28ffed233526b7afb3836c9a05
RifqohR1903056 <rifqohrojiah45@gmail.com>
2022-06-03 10:51:44 +0700

    menambahkan views halaman

--- application/views/v_home.php
+++ application/views/v_home.php
@@ -1,27 +1,26 @@
- <!-- Hero Section Begin -->
-
-    <section class="hero spad set-bg" data-setbg="<?= base_url() ?>template/front-end/img/masjidnya1.jpeg">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="hero__text">
+<!-- Hero Section Begin -->
+                    <div class="col-lg-6">
+                    <div class="home__about__pic">
+                        <img src="<?= base_url() ?>template/front-end/img/plang1.jpeg" alt="">
+                    </div>
+                    <div>
+                        <script type="text/javascript" src="https://www.muslimpro.com/muslimprowidget.js?cityid=1643078&language=id&timeformat=24" async></script>
                     </div>
                 </div>
-            </div>
-        </div>
-    </section>
-    <!-- Hero Section End -->
 
             </div>
         </div>
     </section>
     <!-- Home About Section End -->
-
+    
     <!-- Services Section Begin -->
-    <section class="services spad">
+    <section class="about spad">
         <div class="container">
+            <div class="about__content">
+                <div class="row">
+                    <div class="col-lg-12">
             <div class="section-title">
-                            <h2>Pengumuman Masjid Al-Istiqomah</h2>
+                            <h3>Pengumuman Masjid Al-Istiqomah</h2>
                         </div>
             <div class="row">
 
@@ -29,7 +28,7 @@
                 <div class="col-lg-4 col-md-4 col-sm-6">
                     <div class="services__item">
                         
-                        <img src="<?= base_url()?>template/front-end/img/services/services-1.png" height="50" alt="">
+                        <img src="<?= base_url()?>template/front-end/services/pengumuman.png" height="50" alt="">
                         <h4><?= $value->judul_pengumuman?> </h4>
                         <p><?= $value->isi_pengumuman?></p>
                         <p>
