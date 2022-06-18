bcae48b21bde04a7e4e895ca39954f7673a61604
RifqohR1903056 <rifqohrojiah45@gmail.com>
2022-06-18 13:03:48 +0700

    menambahkan kontak

--- application/views/v_contact.php
+++ application/views/v_contact.php
@@ -1,7 +1,12 @@
  <!-- Contact Section Begin -->
     <section class="contact spad">
         <div class="container">
-            <div class="row">
+            <div class="row">					
+						<div class="col-lg-12">
+							<div class="headeing pb-30">
+								<div class="section-title">
+                            <h3>KONTAK MASJID AL-ISTIQOMAH</h2>
+                        </div>
                 <div class="col-lg-5 col-md-6 col-sm-5">
                     <div class="contact__widget">
                         <div class="contact__widget__item">
@@ -40,13 +45,6 @@
                 </div>
                 <div class="col-lg-6 offset-lg-1 col-md-6 col-sm-7">
                     <div class="contact__form">
-                        <h2>Your question?</h2>
-                        <form action="#">
-                            <input type="text" placeholder="Your Name">
-                            <input type="text" placeholder="Email">
-                            <textarea placeholder="Your Message"></textarea>
-                            <button type="submit">Send Message</button>
-                        </form>
                     </div>
                 </div>
             </div>
