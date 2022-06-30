cc163bc8be3bac35b3c59b6e0cda53ba1b4fd8d7
RifqohR1903056 <rifqohrojiah45@gmail.com>
2022-06-30 10:16:14 +0700

    mengubah tampilan sholat jumat

---
+++ application/views/v_jumat.php
@@ -0,0 +1,47 @@
+
+			<!-- End Intro Section -->
+			<!-- Contact Section -->
+			<section class="padding ptb-xs-40">
+				<div class="container">
+					<div class="row">					
+						<div class="col-lg-12">
+							<div class="headeing pb-30">
+								<div class="section-title">
+                            <h3>INFO SHOLAT JUMAT</h2>
+                        </div>
+								<span class="b-line l-left line-h"></span>
+							</div>			
+							<div class="col-lg-12">
+								<table class="table table-bordered">
+									<thead class="dark-bg">
+										<tr>					
+											<th class="text-center">NO</th>
+                                            <th class="text-center">Tanggal</th>
+											<th class="text-center">Nama Khatib </th>
+											<th class="text-center">Nama Imam</th>
+											<th class="text-center">Nama Muadzin </th>
+										</tr>
+									</thead>
+									<tbody>
+										<?php $no=1; foreach ($jumat as $key => $value) { ?>
+											
+									
+										<tr>
+											<td class="text-center"> <?= $no++ ?> </td>
+                                            <td><?= $value->tgl ?></td>
+											<td><?= $value->nm_khatib?> </td>
+                                            <td><?= $value->nm_imam?> </td>
+                                            <td><?= $value->nm_muadzin?> </td>
+											
+										</tr>
+									<?php 	}?>
+									</tbody>
+								</table>
+							</div>
+						</div>	
+					</div>
+				</div>
+				<!-- Map Section -->
+
+			</section>
+			
