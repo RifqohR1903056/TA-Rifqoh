47fe6756b89846b41440f4e1942cba1645442da1
RifqohR1903056 <rifqohrojiah45@gmail.com>
2022-06-30 10:32:03 +0700

    mengubah tampilan sapra user

--- application/views/v_sapra.php
+++ application/views/v_sapra.php
@@ -1,26 +1,22 @@
 	 <section class="rooms spad">
         <div class="container">
-            <div class="row">
+           
                 <?php foreach ($sapra as $key => $value) { ?>
-                <div class="col-lg-6 p-0 order-lg-2 order-md-2 col-md-6">
-                    <div class="room__pic__slider owl-carousel">
-                      <!--   <div class="room__pic__item set-bg" data-setbg="<?=base_url('sampul/'.$value->sampul)?>" width ="120px"	></div> -->
-                       <figure>
-												<img src="<?=base_url('sampul/'.$value->sampul)?>" width ="50px" alt="">
-											</figure> 
-                    </div>
-                </div>
-                <div class="col-lg-6 p-0 order-lg-1 order-md-1 col-md-6">
-                    <div class="room__text">
-                        <h3>Sarana & Prasarana</h3>
-                        <ul>
-                            <li><span><?= $value->nama_sapra?></li>
-                        </ul>
-                    </div>
-                </div>
+                
+                    <div class="container">
+                        <div class="row">
 
+                            
+                            <div class="card" style="width: 18rem;">
+                                <img src="<?=base_url('sampul/'.$value->sampul)?>" class="card-img-top img-thumbnail" alt="..." >
+                                <div class="card-body">
+                                    <p class="card-text"><?= $value->nama_sapra?></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 				<?php } ?>
-            </div>
+        
         </div>
     </section>
 
