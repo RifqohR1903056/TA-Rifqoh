cbe882e57b1740163ba06f52a2505943861d5ec1
RifqohR1903056 <rifqohrojiah45@gmail.com>
2022-07-12 11:33:28 +0700

    menambahkan model laporan kas

---
+++ application/models/M_lapkas.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class M_lapkas extends CI_Model {
+
+    public function tampil_kas()
+    {
+        return $this->db->get('kas_masjid')->result();
+    }
+    public function tot_rekap()
+    {
+        return $this->db->query('SELECT SUM(masuk) as tot_masuk, SUM(keluar) as tot_keluar FROM kas_masjid')->result();
+    }
+}
+?>
