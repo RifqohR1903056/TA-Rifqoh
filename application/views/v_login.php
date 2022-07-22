5aae451757a3d06d75e4cc12ce1955e49e8fc42f
RifqohR1903056 <rifqohrojiah45@gmail.com>
2022-07-22 19:09:58 +0700

    halaman login

--- application/views/v_login.php
+++ application/views/v_login.php
@@ -49,53 +49,7 @@
               </div>
               <div>
               <button type ="submit" class ="btn btn-success">Login</button>
-                <a class="reset_pass" href="#">Lost your password?</a>
-              </div>
-
-              <div class="clearfix"></div>
-
-              <div class="separator">
-                <p class="change_link">New to site?
-                  <a href="#signup" class="to_register"> Create Account </a>
-                </p>
-
-                <div class="clearfix"></div>
-                <br />
-
-                <div>
-                </div>
-              </div>
-            </form>
-          </section>
-        </div>
-
-        <div id="register" class="animate form registration_form">
-          <section class="login_content">
-            <form>
-              <h1>Create Account</h1>
-              <div>
-                <input type="username" class="form-control" placeholder="Username" required="" />
-              </div>
-              <div>
-                <input type="email" class="form-control" placeholder="Email" required="" />
-              </div>
-              <div>
-                <input type="password" class="form-control" placeholder="Password" required="" />
-              </div>
-              <div>
-              <button type ="submit" class ="btn btn-success">Submit</button>
-              </div>
-
-              <div class="clearfix"></div>
-
-              <div class="separator">
-                <p class="change_link">Already a member ?
-                  <a href="#signin" class="to_register"> Login </a>
-                </p>
-
-                <div class="clearfix"></div>
-                <br />
-
+                
               </div>
             </form>
           </section>
