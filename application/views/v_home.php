26d01f4c03ab0a2a4ed61a43037122d0475fc8b5
RifqohR1903056 <rifqohrojiah45@gmail.com>
2022-08-01 11:58:21 +0700

    user

--- application/views/v_home.php
+++ application/views/v_home.php
@@ -16,18 +16,45 @@
           </div>
         </div>
     </div>
-  </section><!-- End Hero -->
+  </section>
+  <!-- End Hero -->
 
-    <iframe id="iframe" title="prayerWidget" class="widget-m-top" style=" height: 358px; border: 1px solid #ddd;" scrolling="no" src="https://www.islamicfinder.org/prayer-widget/1643078/shafi/11/0/20.0/18.0"> </iframe>
-    
-    <section class="about spad">
-        <div class="container">
-            <div class="about__content">
-                <div class="row">
-                    <div class="col-lg-12">
-            <div class="section-title">
-                            <h3>STRUKTUR ORGANISASI</h2>
-                        </div>
+  <!-- ======= Struktur Section ======= -->
+  <section id="portfolio" class="portfolio">
+      <div class="container-fluid">
+
+        <div class="section-title">
+          <h2>PENGUMUMAN</h2>
+          <h3>Pengumuman Masjid <span>Al-Istiqomah</span></h3>
+          </div>
+            <div class="row">
+
+					<?php $no=1; foreach ($pengumuman as $key => $value) { ?>
+                <div class="col-lg-4 col-md-4 col-sm-6">
+                    <div class="services__item">
+                        <img src="<?= base_url()?>template/front-end/services/pengumuman.png" height="50" alt="">
+                        <h4><?= $value->judul_pengumuman?> </h4>
+                        <p><?= $value->isi_pengumuman?></p>
+                        <p>
+							<h4><span>Tanggal</span> : <?= date("d-m-Y", strtotime($value->tgl))?> </h4>
+						</p>
+                    </div>
+                </div>
+                
+					<?php 	}?>
+            </div>
+        </div>
+    </section>
+    <!-- About Section End -->
+
+
+    <!-- ======= Struktur Section ======= -->
+ <section id="portfolio" class="portfolio">
+      <div class="container-fluid">
+
+        <div class="section-title">
+          <h2>STRUKTUR</h2>
+          <h3>Struktur Organisasi <span>Al-Istiqomah</span></h3>
                
             </div>
             <center>
@@ -36,40 +63,103 @@
             </div>
   </center>
         </div>
-    </div>
-</section>
 <!--//END ABOUT -->
 
 
- <!-- Services Section Begin -->
- <section class="about spad">
-        <div class="container">
-            <div class="about__content">
-                <div class="row">
-                    <div class="col-lg-12">
-            <div class="section-title">
-                            <h3>PENGUMUMAN MASJID AL-ISTIQOMAH</h2>
-                        </div>
+   
+    <!-- ======= Team Section ======= -->
+    <section id="team" class="team">
+      <div class="container-fluid">
+
+        <div class="section-title">
+          <h2>Team</h2>
+          <h3>Our Hard Working <span>Team</span></h3>
+          <p>Ut possimus qui ut temporibus culpa velit eveniet modi omnis est adipisci expedita at voluptas atque vitae autem.</p>
+        </div>
+
+        <div class="row justify-content-center">
+          <div class="col-xl-10">
             <div class="row">
 
-                    <?php $no=1; foreach ($pengumuman as $key => $value) { ?>
-                <div class="col-lg-4 col-md-4 col-sm-6">
-                    <div class="services__item">
-                        
-                        <img src="<?= base_url()?>template/front-end/services/pengumuman.png" height="50" alt="">
-                        <h4><?= $value->judul_pengumuman?> </h4>
-                        <p><?= $value->isi_pengumuman?></p>
-                        <p>
-                        <h4><span>Tanggal</span> : <?= date("d-m-Y", strtotime($value->tgl))?> </h4>
-                            </p>
+              <div class="col-xl-3 col-lg-4 col-md-6">
+                <div class="member">
+                  <img src="template/front-end/assets/img/team/perpus.jpg" class="img-fluid" alt="">
+                  <div class="member-info">
+                    <div class="member-info-content">
+                      <h4>Walter White</h4>
+                      <span>Chief Executive Officer</span>
                     </div>
+                    <div class="social">
+                      <a href=""><i class="bi bi-twitter"></i></a>
+                      <a href=""><i class="bi bi-facebook"></i></a>
+                      <a href=""><i class="bi bi-instagram"></i></a>
+                      <a href=""><i class="bi bi-linkedin"></i></a>
+                    </div>
+                  </div>
                 </div>
+              </div> <!-- End Member Item -->
 
-                    <?php   }?>
+              <div class="col-xl-3 col-lg-4 col-md-6" data-wow-delay="0.1s">
+                <div class="member">
+                  <img src="template/front-end/assets/img/team/parkir.jpg" class="img-fluid" alt="">
+                  <div class="member-info">
+                    <div class="member-info-content">
+                      <h4>Sarah Jhonson</h4>
+                      <span>Product Manager</span>
+                    </div>
+                    <div class="social">
+                      <a href=""><i class="bi bi-twitter"></i></a>
+                      <a href=""><i class="bi bi-facebook"></i></a>
+                      <a href=""><i class="bi bi-instagram"></i></a>
+                      <a href=""><i class="bi bi-linkedin"></i></a>
+                    </div>
+                  </div>
+                </div>
+              </div> <!-- End Member Item -->
+
+              <div class="col-xl-3 col-lg-4 col-md-6" data-wow-delay="0.2s">
+                <div class="member">
+                  <img src="template/front-end/assets/img/team/parkir.jpg" class="img-fluid" alt="">
+                  <div class="member-info">
+                    <div class="member-info-content">
+                      <h4>William Anderson</h4>
+                      <span>CTO</span>
+                    </div>
+                    <div class="social">
+                      <a href=""><i class="bi bi-twitter"></i></a>
+                      <a href=""><i class="bi bi-facebook"></i></a>
+                      <a href=""><i class="bi bi-instagram"></i></a>
+                      <a href=""><i class="bi bi-linkedin"></i></a>
+                    </div>
+                  </div>
+                </div>
+              </div> <!-- End Member Item -->
+
+              <div class="col-xl-3 col-lg-4 col-md-6" data-wow-delay="0.3s">
+                <div class="member">
+                  <img src="template/front-end/assets/img/team/parkir.jpg" class="img-fluid" alt="">
+                  <div class="member-info">
+                    <div class="member-info-content">
+                      <h4>Amanda Jepson</h4>
+                      <span>Accountant</span>
+                    </div>
+                    <div class="social">
+                      <a href=""><i class="bi bi-twitter"></i></a>
+                      <a href=""><i class="bi bi-facebook"></i></a>
+                      <a href=""><i class="bi bi-instagram"></i></a>
+                      <a href=""><i class="bi bi-linkedin"></i></a>
+                    </div>
+                  </div>
+                </div>
+              </div> <!-- End Member Item -->
+
             </div>
+          </div>
         </div>
-    </section>
-    <!-- Services Section End -->
+
+      </div>
+    </section><!-- End Team Section -->
+
 <!-- About Section Begin -->
 <section class="about spad">
         <div class="container">
@@ -108,7 +198,7 @@
 
         <div class="section-title">
           <h2>GALLERY PHOTO</h2>
-          <h3>Check our <span>Gallery Photo</span></h3>
+          <h3>Gallery Photo <span>Al-Istiqomah</span></h3>
           <p>Begitulah Keajaiban Fotografi, Ngga Hanya Mengabadikan Momen Penting Dalam Hidup,Tapi juga Membekukan Kenangan Yang Ingin Diingat.</p>
         </div>
 
@@ -268,15 +358,14 @@
 
         <div class="section-title">
           <h2>Contact</h2>
-          <h3>Get In Touch With <span>Us</span></h3>
-          <p>Ut possimus qui ut temporibus culpa velit eveniet modi omnis est adipisci expedita at voluptas atque vitae autem.</p>
+          <h3>KONTAK KAMI</h3>
         </div>
 
         <div class="row justify-content-center">
           <div class="col-xl-10">
             <div class="row">
 
-              <div class="col-lg-6">
+              <div class="col-lg-12">
 
                 <div class="row justify-content-center">
 
@@ -287,7 +376,7 @@
                   </div>
                   <div class="col-md-6 info d-flex flex-column align-items-stretch">
                     <i class="bx bx-phone"></i>
-                    <h4>Telefon</h4>
+                    <h4>Telephone</h4>
                     <p>+6282 1156 09172 <br>+6281 8069 47762</p>
                   </div>
                   <div class="col-md-6 info d-flex flex-column align-items-stretch">
@@ -297,41 +386,12 @@
                   </div>
                   <div class="col-md-6 info d-flex flex-column align-items-stretch">
                     <i class="bx bx-time-five"></i>
-                    <h4>Working Hours</h4>
-                    <p>Mon - Fri: 9AM to 5PM<br>Sunday: 9AM to 1PM</p>
+                    <h4>Jam Buka</h4>
+                    <p>Senin-Minggu: 04.00 s.d 10.00</p>
                   </div>
 
                 </div>
 
-              </div>
-
-              <div class="col-lg-6">
-                <form action="forms/contact.php" method="post" role="form" class="php-email-form">
-                  <div class="row">
-                    <div class="col-md-6 form-group">
-                      <label for="name">Your Name</label>
-                      <input type="text" name="name" class="form-control" id="name" required>
-                    </div>
-                    <div class="col-md-6 form-group mt-3 mt-md-0">
-                      <label for="email">Your Email</label>
-                      <input type="email" class="form-control" name="email" id="email" required>
-                    </div>
-                  </div>
-                  <div class="form-group mt-3">
-                    <label for="subject">Subject</label>
-                    <input type="text" class="form-control" name="subject" id="subject" required>
-                  </div>
-                  <div class="form-group mt-3">
-                    <label for="message">Message</label>
-                    <textarea class="form-control" name="message" rows="8" required></textarea>
-                  </div>
-                  <div class="my-3">
-                    <div class="loading">Loading</div>
-                    <div class="error-message"></div>
-                    <div class="sent-message">Your message has been sent. Thank you!</div>
-                  </div>
-                  <div class="text-center"><button type="submit">Send Message</button></div>
-                </form>
               </div>
 
             </div>
--- application/views/v_organisasi.php
+++ application/views/v_organisasi.php
@@ -1,16 +1,32 @@
+<!-- About Section Begin -->
+<section class="about spad">
+        <div class="container">
+            <div class="about__content">
+                <div class="row">
+                    <div class="col-lg-13">
+                        <div class="section-title">
+                            <h3>STRUKTUR ORGANISASI</h2>
+                        </div>
+                    </div>
+                   
+                </div>
+            </div>
+
 <section class="rooms spad">
         <div class="container">
 
 <div class="row">
 <?php foreach ($organisasi as $key => $value) { ?>
-  <div class="col-sm-8">
-   
-    <img src="<?=base_url('struktur/'.$value->struktur)?>" class="card-img-top" alt="..." width="900px" height="300px">
-     
+    <center> 
+  <div class="col-sm-6">
+    <img src="<?=base_url('struktur/'.$value->struktur)?>" class="img-fluid about-img" alt="#">
+</div>
+</center>
        
   </div>
   <?php } ?>
 </div>
+
 
         
         </div>
