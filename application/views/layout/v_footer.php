f0c9f112325ee38eaa1ef47077bf0acacb79d92d
RifqohR1903056 <rifqohrojiah45@gmail.com>
2022-08-01 11:57:18 +0700

    layout user

--- application/views/layout/v_footer.php
+++ application/views/layout/v_footer.php
@@ -1,17 +1,6 @@
  <!-- ======= Footer ======= -->
  <footer id="footer">
 
-<div class="footer-newsletter">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-6">
-        <h4>Our Newsletter</h4>
-        <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
-      </div>
-      <div class="col-lg-6">
-        <form action="" method="post">
-          <input type="email" name="email"><input type="submit" value="Subscribe">
-        </form>
       </div>
     </div>
   </div>
@@ -24,31 +13,31 @@
         <div class="row">
 
           <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Useful Links</h4>
+            <h4>Tautan</h4>
             <ul>
               <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
               <li><i class="bx bx-chevron-right"></i> <a href="#">Profile</a></li>
               <li><i class="bx bx-chevron-right"></i> <a href="#">Agenda</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Pengumuman</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Berita</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Laporan KAs</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Kontak</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="<?= base_url('home/pengumuman') ?>">Pengumuman</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="<?= base_url('home/berita') ?>">Berita</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="<?= base_url('home/lapkas') ?>">Laporan Kas</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="<?= base_url('home/contact') ?>">Kontak</a></li>
             </ul>
           </div>
 
           <div class="col-lg-3 col-md-6 footer-contact">
-            <h4>Contact Us</h4>
+            <h4>Kontak Kami</h4>
             <p>
               Masjid Al-Istiqomah <br>
               Desa Lohbener, Kabupaten Indramayu, Jawa Barat<br>
-              <strong>Phone:</strong> +1 5589 55488 55<br>
-              <strong>Email:</strong> info@example.com<br>
+              <strong>Phone:</strong> + 6282 1156 09172 <br> + 6281 8069 47762</p>
+              <strong>Email:</strong> istiqomah@gmail.com <br> remaislohbener@gmail.com</p>
             </p>
 
           </div>
 
           <div class="col-lg-3 col-md-6 footer-info">
-            <h3>About Al-Istiqomah</h3>
+            <h3>Tentang Al-Istiqomah</h3>
             <p>Masjid Al-istiqomah dibangun pada tahun 1970 dan beralamat di Desa Lohbener RT.18 RW.05 Indramayu Jawa Barat.</p>
             <div class="social-links mt-3">
               <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
@@ -67,14 +56,13 @@
 
 <div class="container">
   <div class="copyright">
-    &copy; Copyright <strong><span>Hidayah</span></strong>. All Rights Reserved
+    &copy; Sistem Informasi Manajemen Masjid <strong><span>Al-Istiqomah</span>
   </div>
   <div class="credits">
     <!-- All the links in the footer should remain intact. -->
     <!-- You can delete the links only if you purchased the pro version. -->
     <!-- Licensing information: https://bootstrapmade.com/license/ -->
     <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/hidayah-free-simple-html-template-for-corporate/ -->
-    Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
   </div>
 </div>
 </footer>
