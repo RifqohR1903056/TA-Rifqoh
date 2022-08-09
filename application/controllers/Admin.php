e897191c0e929b418d11c92272fc98620df1f5cb
RifqohR1903056 <rifqohrojiah45@gmail.com>
2022-08-09 21:06:46 +0700

    controller admin

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -4,24 +4,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Admin extends CI_Controller {
 
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('m_kas_keluar');
+        $this->load->model('m_kas');
+    }
+
     public function index()
     {
         $data = array(
             'title' => 'Selamat Datang Admin', 
             'pengumuman' =>$this->db->get('tbl_pengumuman')->num_rows(),
             'berita' =>$this->db->get('tbl_berita')->num_rows(),
-            'kas_keluar' => $this->db->query('SELECT sum(keluar) FROM kas_masjid'),
-           
+            'sapra' =>$this->db->get('tbl_sapra')->num_rows(),
+            'tot_masuk' => $this->m_kas->tot_masuk(),
+            'tot_keluar' => $this->m_kas_keluar->tot_keluar(),
             'isi'  => 'admin/v_home'
-        );
-        // var_dump(coba);
-      
+        );  
         $this->load->view('admin/layout/v_wrapper', $data, FALSE);
     }
 
   
-
-
 
 }
 
